308d3884e13b6da3cbc83ef7abac480f530de803
Peter Newhook <peter.newhook@gmail.com>
2015-04-30 11:30:08 -0400

    adding vim files

---
+++ copy-files.ps1
@@ -0,0 +1,4 @@
+﻿cmd /c mklink %USERPROFILE%\_vimrc %cd%\vim\_vimrc
+cmd /c mklink %USERPROFILE%\_gvimrc %cd%\vim\_gvimrc
+New-Item -ItemType directory -Name colors -Path $env:USERPROFILE\vimfiles\ -Force
+Copy-Item .\vim\pyte.vim $env:USERPROFILE\vimfiles\colors\pyte.vim
---
+++ install-vundle.ps1
@@ -0,0 +1,3 @@
+﻿Push-Location $env:USERPROFILE
+git clone https://github.com/gmarik/Vundle.vim.git vimfiles/bundle/Vundle.vim
+Pop-Location
